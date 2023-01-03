aac96df335bf42bfbb16c51e813cc1b01a2b4741
1018358689 <1018358689@qq.com>
2023-01-04 00:47:44 +0800

    20230104

--- 算法相关/test.py
+++ 算法相关/test.py
@@ -1,6 +1,11 @@
-for x in range(1, 10):
-    for y in range(1, x + 1):
-        print(f'{y}x{x}=={x*y}', end='\t')
-    print('')
-    
-    
+def dfs(n):
+    res = []
+    for x in range(2, n + 1):
+        if not n % x:
+            res.append(x)
+            res += dfs(n // x)
+            break
+    return res
+
+
+print(dfs(99))
--- 算法相关/新手村/webdoit copy.py
+++ 算法相关/新手村/webdoit copy.py
@@ -1,8 +1,9 @@
-from selenium import webdriver
-
+import datetime
 import time
 from random import randint
-import datetime
+
+from selenium import webdriver
+
 # from fake_useragent import UserAgent
 
 # ua = UserAgent()
@@ -22,7 +23,7 @@ def isDuringThatTime(startTime, endTime):
     return start_time <= now_time <= end_time
 
 
-for id in range(311, 312):
+for id in range(297, 340):
     #1.创建Chrome浏览器对象，这会在电脑上在打开一个浏览器窗口
     browser = webdriver.Chrome(executable_path="E:\chromedriver", options=opt)
     #2.通过浏览器向服务器发送URL请求
@@ -31,7 +32,7 @@ for id in range(311, 312):
     # browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
     cnt = 0
     #3.刷新浏览器
-    while not isDuringThatTime("23:58", "23:59") and cnt < 23:
+    while not isDuringThatTime("23:58", "23:59") and cnt < 100:
         cnt += 1
         browser.refresh()
         # for i in range(10):
